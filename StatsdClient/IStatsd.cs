031ca61cb3cb2e66fc88629863eaf125107ac259
Antony Denyer <antonydenyer@gmail.com>
2012-09-03 16:29:05 +0100

    Expose Statsd client public methods using an interface

--- StatsdClient/IStatsd.cs
+++ StatsdClient/IStatsd.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+
 namespace StatsdClient
 {
     public interface IStatsd
     {
-  
+        List<string> Commands { get; }
+        void Send<TCommandType>(string name, int value) where TCommandType : ICommandType;
+        void Send(string name, int value, double sampleRate);
+        void Add<TCommandType>(string name, int value) where TCommandType : ICommandType;
+        void Send<TCommandType>(string name, int value, double sampleRate) where TCommandType : ICommandType;
+        void Add(string name, int value, double sampleRate);
+        void Send(string command);
+        void Send();
+        void Add(Action actionToTime, string statName);
+        void Send(Action actionToTime, string statName);
     }
 }
