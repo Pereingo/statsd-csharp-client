21c156706b12c0199f0cea8f65d85a9a8e17f76d
Anthony Steele <anthony.steele@7digital.com>
2012-10-16 14:43:40 +0100

    Code for consideration - metrics class in the statsd client package

---
+++ StatsdClient/Metrics.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace StatsdClient
+{
+	public static class Metrics
+	{
+	 	private static Statsd _statsD;
+	 	private static string _prefix;
+
+		public static void Configure(MetricsConfig config)
+		{
+			if (config == null)
+			{
+				throw new ArgumentNullException("config");
+			}
+
+			if (! string.IsNullOrEmpty(config.StatsdServerName))
+			{
+				_prefix = config.Prefix;
+				_statsD = new Statsd(new StatsdUDP(config.StatsdServerName, 8125));
+			}
+		}
+
+		public static void Counter(string statName, int value = 1)	
+		{
+			if (_statsD == null)
+			{
+				return;
+			}
+			_statsD.Send<Statsd.Counting>(BuildNamespacedStatName(statName), value);
+		}
+
+		public static void Gauge(string statName, int value)
+		{
+			if (_statsD == null)
+			{
+				return;
+			}
+			_statsD.Send<Statsd.Gauge>(BuildNamespacedStatName(statName), value);
+		}
+
+		public static void Timer(string statName, int value)
+		{
+			if (_statsD == null)
+			{
+				return;
+			}
+
+			_statsD.Send<Statsd.Timing>(BuildNamespacedStatName(statName), value);
+		}
+
+		public static string BuildNamespacedStatName(string statName)
+		{
+			return _prefix + "." + statName;
+		}
+
+		public static IDisposable StartTimer(string name)
+		{
+			return new MetricsTimer(name);
+		}
+	}
+}
---
+++ StatsdClient/MetricsTimer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StatsdClient
+{
+	public class MetricsTimer : IDisposable
+	{
+		private readonly string _name;
+		private readonly Stopwatch _stopWatch;
+		private bool _disposed;
+
+		public MetricsTimer(string name)
+		{
+			_name = name;
+			_stopWatch = new Stopwatch();
+			_stopWatch.Start();
+		}
+
+		public void Dispose()
+		{
+			if (!_disposed)
+			{
+				_disposed = true;
+				_stopWatch.Stop();
+				Metrics.Timer(_name, _stopWatch.ElapsedMilliseconds());
+			}
+		}
+	}
+}
