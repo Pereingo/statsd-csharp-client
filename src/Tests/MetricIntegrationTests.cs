119b7139dbc9bdd63ee1f8135e6c487d6b930521
Elijah Andrews <elijah@datadoghq.com>
2013-06-25 17:40:53 -0400

    Add ability to test Metrics

---
+++ src/Tests/MetricIntegrationTests.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Threading;
+using System.Configuration;
+using NUnit.Framework;
+using StatsdClient;
+using Tests.Helpers;
+
+
+namespace Tests
+{
+    [TestFixture]
+    public class MetricIntegrationTests
+    {
+        private UdpListener udpListener;
+        private Thread listenThread;
+        private int serverPort = Convert.ToInt32(ConfigurationManager.AppSettings["StatsdServerPort"]);
+        private string serverName = ConfigurationManager.AppSettings["StatsdServerName"];
+
+        [TestFixtureSetUp]
+        public void SetUpUdpListener() 
+        {
+            udpListener = new UdpListener(serverName, serverPort);
+            var metricsConfig = new MetricsConfig { StatsdServerName = serverName };
+            StatsdClient.Metrics.Configure(metricsConfig);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDownUdpListener() 
+        {
+            udpListener.Dispose();
+        }
+
+        [SetUp]
+        public void StartUdpListenerThread()
+        {
+            listenThread = new Thread(new ThreadStart(udpListener.Listen));
+            listenThread.Start();
+        }
+
+        // Test helper. Waits until the listener is done receiving a message,
+        // then asserts that the passed string is equal to the message received.
+        private void AssertWasReceived(string shouldBe)
+        {
+                // Stall until the the listener receives a message or times out 
+                while(listenThread.IsAlive);
+                Assert.AreEqual(shouldBe, udpListener.GetAndClearLastMessage());
+
+        }
+
+        [Test]
+        public void _udp_listener_sanity_test()
+        {
+            var client = new StatsdUDP(ConfigurationManager.AppSettings["StatsdServerName"],
+                                       Convert.ToInt32(ConfigurationManager.AppSettings["StatsdServerPort"]));
+            client.Send("iamnotinsane!");
+            AssertWasReceived("iamnotinsane!");
+
+        }
+
+        [Test]
+        public void counter()
+        {
+            Metrics.Counter("counter");
+            AssertWasReceived("counter:1|c");
+        }
+    }
+}
+
