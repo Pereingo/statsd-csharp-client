871a7fec5ac64ffc47f8078dfd1ab629c6ca3a60
Elijah Andrews <elijah@datadoghq.com>
2013-06-26 16:25:50 -0400

    Use spaces to indent Metrics.cs

--- src/StatsdClient/Metrics.cs
+++ src/StatsdClient/Metrics.cs
@@ -2,20 +2,20 @@
 
 namespace StatsdClient
 {
-	public static class Metrics
-	{
-	 	private static Statsd _statsD;
-	 	private static string _prefix;
+    public static class Metrics
+    {
+        private static Statsd _statsD;
+        private static string _prefix;
 
-		public static void Configure(MetricsConfig config)
-		{
-			if (config == null)
-				throw new ArgumentNullException("config");
+        public static void Configure(MetricsConfig config)
+        {
+            if (config == null)
+                throw new ArgumentNullException("config");
 
             if (string.IsNullOrEmpty(config.StatsdServerName))
                 throw new ArgumentNullException("config.StatsdServername");
 
-			_prefix = config.Prefix;
+            _prefix = config.Prefix;
 
             int port;
             if (config.StatsdPort > 0)
@@ -24,25 +24,25 @@ namespace StatsdClient
                 port = 8125;
     
             _statsD = new Statsd(new StatsdUDP(config.StatsdServerName, port));
-		}
+        }
 
-		public static void Counter(string statName, int value = 1)	
-		{
-			if (_statsD == null)
-			{
-				return;
-			}
-			_statsD.Send<Statsd.Counting>(BuildNamespacedStatName(statName), value);
-		}
+        public static void Counter(string statName, int value = 1)  
+        {
+            if (_statsD == null)
+            {
+                return;
+            }
+            _statsD.Send<Statsd.Counting>(BuildNamespacedStatName(statName), value);
+        }
 
-		public static void Gauge(string statName, int value)
-		{
-			if (_statsD == null)
-			{
-				return;
-			}
-			_statsD.Send<Statsd.Gauge>(BuildNamespacedStatName(statName), value);
-		}
+        public static void Gauge(string statName, int value)
+        {
+            if (_statsD == null)
+            {
+                return;
+            }
+            _statsD.Send<Statsd.Gauge>(BuildNamespacedStatName(statName), value);
+        }
 
         public static void Histogram (string statName, int value)
         {
@@ -62,54 +62,54 @@ namespace StatsdClient
             _statsD.Send<Statsd.Set>(BuildNamespacedStatName (statName), value);
         }
 
-		public static void Timer(string statName, int value)
-		{
-			if (_statsD == null)
-			{
-				return;
-			}
+        public static void Timer(string statName, int value)
+        {
+            if (_statsD == null)
+            {
+                return;
+            }
 
-			_statsD.Send<Statsd.Timing>(BuildNamespacedStatName(statName), value);
-		}
+            _statsD.Send<Statsd.Timing>(BuildNamespacedStatName(statName), value);
+        }
 
-		public static IDisposable StartTimer(string name)
-		{
-			return new MetricsTimer(name);
-		}
+        public static IDisposable StartTimer(string name)
+        {
+            return new MetricsTimer(name);
+        }
 
-		public static void Time(Action action, string statName) 
-		{
-			if (_statsD == null)
-			{
-				action();
-			}
-			else
-			{
-				_statsD.Send(action, BuildNamespacedStatName(statName));
-			}
-		}
+        public static void Time(Action action, string statName) 
+        {
+            if (_statsD == null)
+            {
+                action();
+            }
+            else
+            {
+                _statsD.Send(action, BuildNamespacedStatName(statName));
+            }
+        }
 
-		public static T Time<T>(Func<T> func, string statName)
-		{
-			if (_statsD == null)
-			{
-				return func();
-			}
+        public static T Time<T>(Func<T> func, string statName)
+        {
+            if (_statsD == null)
+            {
+                return func();
+            }
 
-			using (StartTimer(statName))
-			{
-				return func();
-			}
-		}
+            using (StartTimer(statName))
+            {
+                return func();
+            }
+        }
 
-		private static string BuildNamespacedStatName(string statName)
-		{
-			if (string.IsNullOrEmpty(_prefix))
-			{
-				return statName;
-			}
+        private static string BuildNamespacedStatName(string statName)
+        {
+            if (string.IsNullOrEmpty(_prefix))
+            {
+                return statName;
+            }
 
-			return _prefix + "." + statName;
-		}
-	}
+            return _prefix + "." + statName;
+        }
+    }
 }
