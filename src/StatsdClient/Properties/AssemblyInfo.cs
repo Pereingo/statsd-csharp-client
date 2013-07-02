210088294a67c39a99f4330650b0a7cd2426e3d6
Elijah Andrews <elijah@datadoghq.com>
2013-07-02 14:56:03 -0400

    Update AssemblyInfo for initial release

--- src/StatsdClient/Properties/AssemblyInfo.cs
+++ src/StatsdClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StatsdClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DogStatsD for C#")]
+[assembly: AssemblyDescription("A C# DogStatsD client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Datadog")]
 [assembly: AssemblyProduct("StatsdClient")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ecebfa48-5557-4fe6-84a6-c0b1e3ece14c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.14")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// This project follows http://semver.org/ for versioning
+[assembly: AssemblyVersion("1.0.0")]
