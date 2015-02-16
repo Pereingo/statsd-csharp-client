29e98e140bec8cfbbf0590338361f7775d116004
Darrell Mozingo <darrell@mozingo.net>
2015-02-16 16:37:33 +0000

    Cleaned up exception message to show what's in the config right now

--- src/Tests/UDPSmokeTests.cs
+++ src/Tests/UDPSmokeTests.cs
@@ -9,20 +9,20 @@ namespace Tests
     [TestFixture]
     public class UDPSmokeTests
     {
-        private static readonly int ServerPort = Convert.ToInt32(ConfigurationManager.AppSettings["StatsdServerPort"]);
-        private static readonly string ServerName = ConfigurationManager.AppSettings["StatsdServerName"];
+        private static readonly int _serverPort = Convert.ToInt32(ConfigurationManager.AppSettings["StatsdServerPort"]);
+        private static readonly string _serverName = ConfigurationManager.AppSettings["StatsdServerName"];
 
         [Test]
         public void Sends_a_counter()
         {
             try
             {
-                var client = new StatsdUDP(ServerName, ServerPort);
+                var client = new StatsdUDP(_serverName, _serverPort);
                 client.Send("socket2:1|c");
             }
             catch(SocketException ex)
             {
-                Assert.Fail("Socket Exception, have you set up your Statsd name and port? Error: {0}", ex.Message);
+                Assert.Fail("Socket Exception, have you setup your Statsd name and port? It's currently '{0}:{1}'. Error: {2}", _serverName, _serverPort, ex.Message);
             }
         }
     }
