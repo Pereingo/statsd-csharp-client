1a6d5fb44a252fb02564a3ad8296281b9ab42e11
Darrell Mozingo <darrell@mozingo.net>
2015-10-09 12:54:32 +0100

    Description and comment deletion

--- src/StatsdClient/Properties/AssemblyInfo.cs
+++ src/StatsdClient/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StatsdClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Statsd Client")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StatsdClient")]
@@ -21,15 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ecebfa48-5557-4fe6-84a6-c0b1e3ece14c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
