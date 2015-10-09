f03c9f9a8b8bb3d48f04c55d44e779c992f4b011
Darrell Mozingo <darrell@mozingo.net>
2015-10-09 17:54:24 +0100

    Tab -> space for consistency.

--- src/Tests/MetricsTests.cs
+++ src/Tests/MetricsTests.cs
@@ -4,12 +4,12 @@ using StatsdClient;
 
 namespace Tests
 {
-	public class MetricsTests
-	{
-		[Test]
-		public void throws_when_configured_with_a_null_configuration()
-		{
-			Assert.Throws<ArgumentNullException>(() => Metrics.Configure(null));
-		}
-	}
+    public class MetricsTests
+    {
+        [Test]
+        public void throws_when_configured_with_a_null_configuration()
+        {
+            Assert.Throws<ArgumentNullException>(() => Metrics.Configure(null));
+        }
+    }
 }
