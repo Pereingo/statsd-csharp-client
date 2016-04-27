496940211b42cc33f9884dcb34eef5ff3e009f14
pekiZG <petar.slat@gmail.com>
2016-04-28 00:30:37 +0200

    Implemented IDisposable interface

--- src/StatsdClient/StatsdTCP.cs
+++ src/StatsdClient/StatsdTCP.cs
@@ -47,6 +47,7 @@ namespace StatsdClient
         }
 
         #region IDisposable Support
+
         private bool disposedValue = false; // To detect redundant calls
 
         protected virtual void Dispose(bool disposing)
@@ -55,30 +56,32 @@ namespace StatsdClient
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects).
+                    if (_clientSocket != null)
+                    {
+                        try
+                        {
+                            _clientSocket.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
                 disposedValue = true;
             }
         }
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~StatsdTCP() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
+        ~StatsdTCPClient() {
+          Dispose(false);
+        }
 
-        // This code added to correctly implement the disposable pattern.
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
+            GC.SuppressFinalize(this);
         }
+
         #endregion
     }
 }
